0a64ea1523561a6ed310a98746f6379710f55124
domenslak <domenslak08@gmail.com>
2024-12-28 21:58:41 +0100

    First commit
    Added grid and ability to add cells.

---
+++ conwaysgameoflife/main.py
@@ -0,0 +1,62 @@
+import pygame, math
+
+
+width = 500
+height = 500
+square = 20
+gwidth = width // square
+gheight = height // square
+
+
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+running = True
+
+
+
+background = (0,0,0)
+grid = (40,40,40)
+alive = (255,255,255)
+
+position = []
+
+
+
+
+
+def drawgrid():
+    for i in range(height):
+        pygame.draw.line(screen, grid, (0, i * square), (width, i * square))
+    for j in range(width):
+        pygame.draw.line(screen, grid, (j * square,0), (j * square, height))
+
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            col = (math.ceil(event.pos[0] / square) * square) - square
+            row = (math.ceil(event.pos[1] / square) * square) - square
+            position.append((col, row))
+            print(position, event)
+
+
+
+    # fill the screen with a color to wipe away anything from last frame
+    pygame.display.set_caption("Game of life")
+
+
+    # RENDER YOUR GAME HERE
+    drawgrid()
+    for x, y in position:
+        pygame.draw.rect(screen, alive, (x, y, square, square))
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+    pygame.display.update()
+    clock.tick(12)  # limits FPS to 60
+
+pygame.quit()
